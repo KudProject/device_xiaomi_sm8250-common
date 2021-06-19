9aecd98560445cc78bdd116b1af73d9a67f508b6
Albert I <kras@raphielgang.org>
2021-06-19 15:10:49 +0800

    sm8250-common: Squash update blobs from CAF
    * system: LA.QSSI.11.0.r1-12100-qssi.0
    * vendor: LA.UM.9.12.r1-10800-SMxx50.0 + LA.UM.9.12.r1-11500-SMxx50.0
    * WFD: LA.UM.9.16.r1-05700-MANNAR.0
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- kona.mk
+++ kona.mk
@@ -124,8 +124,10 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    com.dsi.ant@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_PACKAGES += \
     init.mi.btmac.sh
