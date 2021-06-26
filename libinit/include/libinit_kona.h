e7595c640e4f3952db9e42a5d97fd3a6201979f4
Albert I <kras@raphielgang.org>
2021-06-27 01:13:08 +0800

    sm8250-common: libinit: Allow overriding product name as well
    Different devices set different properties here too.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- libinit/include/libinit_kona.h
+++ libinit/include/libinit_kona.h
@@ -18,6 +18,7 @@ typedef struct variant_info {
     std::string device;
     std::string marketname;
     std::string model;
+    std::string name;
     std::string build_description;
     std::string build_fingerprint;
 } variant_info_t;
